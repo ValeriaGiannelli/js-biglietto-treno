56558e3328bcf68b86c18f90ec3acc03213b4b82
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-06-04 14:10:14 +0200

    Macro compiti

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero.
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// va applicato uno sconto del 20% per i minorenni
+// va applicato uno sconto del 40% per gli over 65.
+// L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
+
+
+
+// Chiedere all’utente il numero di chilometri che vuole percorrere
+
+
+
+// Chiedere all’utente l’età
+
+
+
+// calcolo prezzo
+
+    // se età minore 18 -> sconto 20%
+
+
+    // se età maggiore 65 -> sconto 40%
+
+
+    // altrimenti prezzo intero 0.21 * numero chilometri
+
+
+
+// arrotondo il prezzo con due decimali
+
+
+
+// output a schermo: "il prezzo del biglietto è: "
